93dd6580540be57850e84ccb741873ca141a8687
theprudencio <00001098396133SP@aluno.educacao.sp.gov.br>
2024-11-19 08:57:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Bosnia e Herzegovina?',
+    'A capital é Saravejo'
+)
+
+criaCartao(
+    'Conhecimentos básicos',
+    'O que é um Pedro Evangelista?',
+    'Um Pedro evangelista é um amigo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Oi em ingles é bye (BAI)'
+)
